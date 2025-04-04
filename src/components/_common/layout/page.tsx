175b5eb1fa8ed083d6fe9414a5d7debc64fa78b5
Xopher <xopher@muuvlabs.com>
2025-04-03 23:03:25 -0700

    feat: Update Page component to use ActionButtonProps for improved action button handling

--- src/components/_common/layout/page.tsx
+++ src/components/_common/layout/page.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { PageLayout } from '@/components/_common/layout/page-layout';
+import {
+  ActionButtonProps,
+  PageLayout
+} from '@/components/_common/layout/page-layout';
 
 export interface StandardPageProps {
   title: string;
-  actionButton?: {
-    label: string;
-    onClick: () => void;
-  };
+  actionButton?: ActionButtonProps;
   variant?: 'default' | 'container';
   children: React.ReactNode;
 }
 
-export function Page({ title, actionButton, children, variant }: StandardPageProps) {
+export function Page({
+  title,
+  actionButton,
+  children,
+  variant
+}: StandardPageProps) {
   return (
     <PageLayout title={title} actionButton={actionButton}>
       {variant === 'container' ? (
-        <div className="container mx-auto px-0 py-6">
-          {children}
-        </div>
-      ) : children}
+        <div className="container mx-auto px-0 py-6">{children}</div>
+      ) : (
+        children
+      )}
     </PageLayout>
-  )
+  );
 }
