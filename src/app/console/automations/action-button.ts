b12b4f6f7660e41cf58af78165ba5cc1731c99a2
Xopher <xopher@muuvlabs.com>
2025-04-03 23:01:39 -0700

    feat: Add ActionButton component for creating new automations

---
+++ src/app/console/automations/action-button.ts
@@ -0,0 +1,8 @@
+'use client'
+
+import { ActionButtonProps } from "@/components/_common/layout"
+
+export default {
+  label: 'New Automation',
+  href:'/console/automations/create' 
+} as ActionButtonProps 
